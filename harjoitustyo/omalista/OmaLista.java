92cc407d6b1367e4551383a52fdd5237be60c001
Oskari Kuikka <oskari.kuikka@tuni.fi>
2020-04-11 17:27:53 +0300

    Added OmaLista method lisää

--- harjoitustyo/omalista/OmaLista.java
+++ harjoitustyo/omalista/OmaLista.java
@@ -1,8 +1,31 @@
 package harjoitustyo.omalista;
 
+import harjoitustyo.apulaiset.Ooperoiva;
 import java.util.LinkedList;
 
-public class OmaLista<E> extends LinkedList<E>{
+public class OmaLista<E> extends LinkedList<E> implements Ooperoiva<E>{
 
-    
+    // Toteutetaan pakollinen method void lisää Ooperoiva luokasta
+    @SuppressWarnings("unchecked")
+    @Override
+    public void lisää(E uusi) throws IllegalArgumentException {
+        if (!(uusi instanceof Comparable)) {
+            throw new IllegalArgumentException();
+        }
+        if (this.size() == 0) {
+            this.add(uusi);
+        } else {
+
+            for (int i = 0; i < this.size(); i++) {
+                if (((Comparable) this.get(i)).compareTo(uusi) > 0) {
+                    this.add(i, uusi);
+                    break;
+                } else if(i == this.size() - 1){
+                    this.addLast( uusi);
+                    break;
+                }
+
+            }
+        }
+    }
 }
